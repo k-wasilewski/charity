2576199821759b96a367edbfba833b29e1232aa1
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-26 09:51:57 +0200

    some mysql config fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,16 @@
-
+ALTER TABLE institutions CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 INSERT INTO institutions(name, description) VALUES("Dbam o Zdrowie", "Cel i misja: Pomoc dzieciom z ubogich rodzin.");
 INSERT INTO institutions(name, description) VALUES("Dla dzieci", "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.");
 INSERT INTO institutions(name, description) VALUES("A kogo", "Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.");
 INSERT INTO institutions(name, description) VALUES("Bez domu", "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania");
 
+ALTER TABLE donations CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 INSERT INTO donations(quantity, institution_id, pick_up_time, pick_up_date, created, street, city, zip_code) VALUES(5, 1, "12:32", "2020-12-01", "2020-12-01", "frefr", "Warszawa", "02-776");
 INSERT INTO donations_categories VALUES(1, 1);
 INSERT INTO donations(quantity, institution_id, pick_up_time, pick_up_date, created) VALUES(2, 1, "12:32", "2020-12-01", "2020-12-01");
 INSERT INTO donations(quantity, institution_id, pick_up_time, pick_up_date, created) VALUES(7, 2, "12:32", "2020-12-01", "2020-12-01");
 
+ALTER TABLE categories CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 INSERT INTO categories(name) VALUES("zabawki");
 INSERT INTO categories(name) VALUES("ubrania");
 INSERT INTO categories(name) VALUES("jedzenie");
