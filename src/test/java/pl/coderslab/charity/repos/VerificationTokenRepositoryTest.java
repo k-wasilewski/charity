365eefe5d64569c07258961158dc2a454bcb9c96
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-27 16:47:53 +0200

    VerificationTokenRepositoryTest passed

---
+++ src/test/java/pl/coderslab/charity/repos/VerificationTokenRepositoryTest.java
@@ -0,0 +1,78 @@
+package pl.coderslab.charity.repos;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.KafkaContainer;
+import pl.coderslab.charity.entities.VerificationToken;
+import pl.coderslab.charity.kafka.KafkaConsumerConfig;
+import pl.coderslab.charity.kafka.KafkaProducerConfig;
+import pl.coderslab.charity.kafka.KafkaTopicConfig;
+import pl.coderslab.charity.security.entities.User;
+import pl.coderslab.charity.security.repos.UserRepository;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerificationTokenRepositoryTest {
+    @Autowired
+    VerificationTokenRepository verificationTokenRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @ClassRule
+    public static KafkaContainer kafkaContainer = new KafkaContainer();
+
+    @BeforeClass
+    public static void setKafkaContainerName() {
+        kafkaContainer.setNetworkAliases(Arrays.asList("kafka"));
+        KafkaConsumerConfig.setUrl(kafkaContainer.getBootstrapServers());
+        KafkaProducerConfig.setUrl(kafkaContainer.getBootstrapServers());
+        KafkaTopicConfig.setUrl(kafkaContainer.getBootstrapServers());
+    }
+
+    @Test
+    @Transactional
+    public void findByToken() {
+        //given
+        User user = new User();
+        user.setUsername("test user1");
+        userRepository.save(user);
+        VerificationToken verificationToken = new VerificationToken();
+        String token = "test token1";
+        verificationToken.setUser(user);
+        verificationToken.setToken(token);
+        verificationTokenRepository.save(verificationToken);
+
+        //when, then
+        assertEquals(verificationToken, verificationTokenRepository.findByToken(token));
+    }
+
+    @Test
+    @Transactional
+    public void findByUser() {
+        //given
+        User user = new User();
+        user.setUsername("test user2");
+        userRepository.save(user);
+        VerificationToken verificationToken = new VerificationToken();
+        String token = "test token2";
+        verificationToken.setUser(user);
+        verificationToken.setToken(token);
+        verificationTokenRepository.save(verificationToken);
+
+        //when, then
+        assertEquals(verificationToken, verificationTokenRepository.findByUser(user));
+    }
+}
+
