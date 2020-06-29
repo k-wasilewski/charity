6b6103a0318a3eb1fbd24427c956d677a44eaf1e
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-29 17:20:05 +0200

    InstitutionsControllerTest passed

---
+++ src/test/java/pl/coderslab/charity/controllers/admin/InstitutionsControllerTest.java
@@ -0,0 +1,118 @@
+package pl.coderslab.charity.controllers.admin;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.KafkaContainer;
+import pl.coderslab.charity.entities.Institution;
+import pl.coderslab.charity.kafka.KafkaConsumerConfig;
+import pl.coderslab.charity.kafka.KafkaProducerConfig;
+import pl.coderslab.charity.kafka.KafkaTopicConfig;
+import pl.coderslab.charity.security.entities.User;
+import pl.coderslab.charity.security.repos.UserRepository;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class InstitutionsControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    UserRepository userRepository;
+
+    @ClassRule
+    public static KafkaContainer kafkaContainer = new KafkaContainer();
+
+    @BeforeClass
+    public static void setKafkaContainerName() {
+        kafkaContainer.setNetworkAliases(Arrays.asList("kafka"));
+        KafkaConsumerConfig.setUrl(kafkaContainer.getBootstrapServers());
+        KafkaProducerConfig.setUrl(kafkaContainer.getBootstrapServers());
+        KafkaTopicConfig.setUrl(kafkaContainer.getBootstrapServers());
+    }
+
+    @Test
+    @WithUserDetails("admin1@admin.pl")
+    public void testA_institsView() throws Exception {
+        mockMvc.perform(get("/admin/institutions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/admin-institutions"))
+                .andExpect(model().attribute("institutions", hasSize(4)))
+                .andExpect(model().attribute("username",
+                        userRepository.findByUsername("admin1@admin.pl").getUsername()))
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("admin1@admin.pl")
+    public void testB_editView() throws Exception {
+        mockMvc.perform(get("/admin/institutions/edit?id=1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/admin-institution-edit"))
+                .andExpect(model().attribute("inst",
+                        hasProperty("name", is("Dbam o Zdrowie"))))
+                .andExpect(model().attribute("username",
+                        userRepository.findByUsername("admin1@admin.pl").getUsername()))
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("admin1@admin.pl")
+    public void testC_edit() throws Exception {
+        Institution instit = new Institution();
+        instit.setName("Test institution");
+
+        mockMvc.perform(post("/admin/instututions/edit")
+                .flashAttr("inst", instit))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/admin-institutions"))
+                .andExpect(model().attribute("institutions",
+                        hasItem(hasProperty("name", equalTo("Test institution")))))
+                .andExpect(model().attribute("username",
+                        userRepository.findByUsername("admin1@admin.pl").getUsername()))
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("admin1@admin.pl")
+    public void testD_del() throws Exception {
+        mockMvc.perform(get("/admin/institutions/del?id=5"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/admin-institutions"))
+                .andExpect(model().attribute("institutions",
+                        not(hasItem(hasProperty("name", equalTo("Test institution"))))))
+                .andExpect(model().attribute("username",
+                        userRepository.findByUsername("admin1@admin.pl").getUsername()))
+                .andReturn();
+    }
+
+    @Test
+    @WithUserDetails("admin1@admin.pl")
+    public void testE_add() throws Exception {
+        mockMvc.perform(get("/admin/institutions/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/admin-institution-edit"))
+                .andExpect(model().attribute("add", true))
+                .andExpect(model().attribute("inst", notNullValue()))
+                .andExpect(model().attribute("username",
+                        userRepository.findByUsername("admin1@admin.pl").getUsername()))
+                .andReturn();
+    }
+}
