76aa1aa75855054e3ee11f792ff15e6d0822c65f
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-30 18:16:56 +0200

    RoleRepositoryTest passed

---
+++ src/test/java/pl/coderslab/charity/security/repos/RoleRepositoryTest.java
@@ -0,0 +1,48 @@
+package pl.coderslab.charity.security.repos;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.KafkaContainer;
+import pl.coderslab.charity.entities.Category;
+import pl.coderslab.charity.kafka.KafkaConsumerConfig;
+import pl.coderslab.charity.kafka.KafkaProducerConfig;
+import pl.coderslab.charity.kafka.KafkaTopicConfig;
+import pl.coderslab.charity.repos.CategoryRepository;
+import pl.coderslab.charity.security.entities.Role;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoleRepositoryTest {
+    @Autowired
+    RoleRepository roleRepository;
+
+    @ClassRule
+    public static KafkaContainer kafkaContainer = new KafkaContainer();
+
+    @BeforeClass
+    public static void setKafkaContainerName() {
+        kafkaContainer.setNetworkAliases(Arrays.asList("kafka"));
+        KafkaConsumerConfig.setUrl(kafkaContainer.getBootstrapServers());
+        KafkaProducerConfig.setUrl(kafkaContainer.getBootstrapServers());
+        KafkaTopicConfig.setUrl(kafkaContainer.getBootstrapServers());
+    }
+
+    @Test
+    @Transactional
+    public void findByName() {
+        assertNotNull(roleRepository.findByName("ROLE_USER"));
+    }
+}
+
